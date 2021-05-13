f6e8e05e52aea3b6ad7d78209352dbe805c104f7
gabycosta04 <ccosta@frba.utn.edu.ar>
2021-05-13 14:55:24 -0300

    REALIZO refactorizacion en base a el movimiento, crando asi con herencia dos tipos de movimientos diferentes, donde cada una tiene sus metodos particulares

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -13,7 +13,10 @@ import java.util.stream.Stream;
 public class Cuenta {
 
   private double saldo;
-  private List<Movimiento> movimientos = new ArrayList<>();
+  //DECIDO QUE PARA SER MAS FACIL y no tener que andar preguntando si es un deposito o no, hacer dos diferentes tipos de listas, una para extracciones y otra para depositos
+  //private List<Movimiento> movimientos = new ArrayList<>();
+  private List<Extraccion> extracciones = new ArrayList<>();
+  private List<Deposito> depositos = new ArrayList<>();
 
 
 
@@ -21,28 +24,24 @@ public class Cuenta {
   //Aca estas haciendo repeticion de logica, ya que antes establecimos que el saldo tenia el valor = 0, por lo tanto no tendria sentido reasignarle el mismo valor
   //PROPOSICION
 
-  public Cuenta(double montoInicial ) {
+  public Cuenta(double montoInicial) {
     saldo = montoInicial;
   }
-
-
-
 
 
 
   //GETTERS Y SETTERS, habria que analizar si es ncesario ver todos, ya que si no tienen funcionalidad para otras clases, no tendria logica declararlos todos
   //los setters de movimientos y saldos no son necesarios ya que no los vamos a utilizar fuera
 
-  public List<Movimiento> getMovimientos() {
-    return movimientos;
-  }
+
 
   public double getSaldo() {
     return saldo;
   }
 
-
-
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
 
   //CODE SMELL: Duplicated code en poner y sacar ya que hacen cosas de codigo muy parecidas
   //otros dos posibles code smells sean estos dos que analiza diferentes condiciones y que luego ejecuta una excecpion diferente por cada uno
@@ -52,16 +51,18 @@ public class Cuenta {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
   }
+
+
   //otro code smell, ya que la funciones de agregateA(Cuenta cuenta) en monedero esta generando acoplamiento de mas entre las clases, ya que no deberia encargarse el movimiento de agregarse a la lista, sino que deberia agregarlo directamente la cuenta
   //CODE SMELL: MISPLACED METHOD
   //PROPOSICION:
-  public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
-    Movimiento movimiento = new Movimiento(fecha, cuanto, esDeposito);
-    movimientos.add(movimiento);
+  public void agregarExtraccion(LocalDate fecha, double cuanto) {
+    Extraccion movimiento = new Extraccion(fecha, cuanto);
+    extracciones.add(movimiento);
   }
-
-  public boolean masMovimientosQueLimiteEstablecido(Stream<Movimiento> movimientos, int limite){
-    return movimientos.count() >= limite;
+  public void agregarDeposito(LocalDate fecha, double cuanto) {
+    Deposito movimiento = new Deposito(fecha, cuanto);
+    depositos.add(movimiento);
   }
 
 
@@ -74,42 +75,41 @@ public class Cuenta {
     //LE ESTAMOS DANDO MUCHA RESPONSABILIDAD A LA CLASE NUESTRA PARA CALCULAR LA CANTIDAD DE MOVIMIENTOS SI ES UN DEPOSITO,
     //CODE SMELL: FEATURE ENVY
     //PROPOSICION:
-    if (this.masMovimientosQueLimiteEstablecido(movimientos.stream().filter(movimiento -> movimiento.isDeposito()),3)) {
+    if (depositos.size() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
-
-    this.agregarMovimiento(LocalDate.now(), cuanto, true);
+    this.agregarDeposito(LocalDate.now(), cuanto);
     this.saldo += cuanto;
   }
 
 
   public void sacar(double cuanto) {
 
-
     validarMontoPositivo(cuanto);
-
     if (getSaldo() - cuanto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
 
-    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+    double montoExtraidoHoy = this.getMontoExtraidoA(LocalDate.now());
     double limite = 1000 - montoExtraidoHoy;
 
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    this.agregarMovimiento(LocalDate.now(), cuanto, false);
+    this.agregarExtraccion(LocalDate.now(), cuanto);
     this.saldo -= cuanto;
+
   }
 
 
 
   public double getMontoExtraidoA(LocalDate fecha) {
-    //TODO CODE SMELL: FEATURE ENVY porque aca se le estan enviando demasiados mensajes a la clase de movimiento, esta funcion deberia resolverla en la clase de movmiento
-    return movimientos.stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+    //CODE SMELL: FEATURE ENVY porque aca se le estan enviando demasiados mensajes a la clase de movimiento, esta funcion deberia resolverla en la clase de movmiento
+    //PROPOSICION
+    return extracciones.stream()
+        .filter(movimiento -> movimiento.esDeLaFecha(fecha))
         .mapToDouble(movimiento -> movimiento.getMonto())
         .sum();
   }
--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -2,77 +2,70 @@ package dds.monedero.model;
 
 import java.time.LocalDate;
 
-public class Movimiento {
+
+
+//AL HABER SOLO DOS TIPOS DE MOVIME
+abstract public class Movimiento {
   private LocalDate fecha;
-  //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
-  //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
-
-
   private double monto;
-  private boolean esDeposito;
-
 
   //NO ES NECESARIA ESTA FUNCION, YA QUE estamos dandole responsabilidad a el movimiento de agregarse en la cuenta,
   // cuando en realidad, la cuenta es la que tiene que agregar el movimiento
   //CODE SMELL agregateA(Cuenta cuenta):  posible LARGE CLASS ya que le estamos dando responsabilidad de mas a el movimiento?
 
-
-
-
   //EN VEZ DE TENER UNA VARIABLE BOOLEANA DE SI ES DEPOSITO, le daria mejor esa responsabilidad a una clase
   //que tenga la responsabilidad de realizar ciertas tareas
-  //TODO CODE SMELL: PRIMITIVE OBSESSION? ya que estamos representando con booleano, cuando podria ser un obetjo con comportamiento
+  //CODE SMELL: PRIMITIVE OBSESSION? ya que estamos representando con booleano, cuando podria ser un obetjo con comportamiento
 
-  public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
+  public Movimiento(LocalDate fecha, double monto) {
     this.fecha = fecha;
     this.monto = monto;
-    this.esDeposito = esDeposito;
   }
 
   public double getMonto() {
     return monto;
   }
 
-  public LocalDate getFecha() {
-    return fecha;
-  }
-
-
-
-
-  //ACA HAY LOGICA REPETIDA, ya que estas analizan lo mismo solo que de formaContraria
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
-  //
-
-
-
 
   public boolean esDeLaFecha(LocalDate fecha) {
     return this.fecha.equals(fecha);
   }
 
-  public boolean isDeposito() {
-    return esDeposito;
+  //ACA HAY LOGICA REPETIDA, ya que estas analizan lo mismo solo que de formaContraria
+  abstract public double calcularValor(double saldo);
+}
+
+
+
+
+
+class Deposito extends Movimiento{
+  public Deposito(LocalDate fecha, double monto) {
+    super(fecha, monto);
   }
-
-  public boolean isExtraccion() {
-    return !esDeposito;
+  public double calcularValor(double saldo){
+    return saldo + this.getMonto();
   }
-
-
-
-  //ADEMAS NO ES CORRECTO PREGUNTAR SI SE TRATA DE UN DEPOSITO O NO: TODO CODE SMELL: TYPE TEST?
-  public double calcularValor(Cuenta cuenta) {
-    if (esDeposito) {
-      return cuenta.getSaldo() + getMonto();
-    } else {
-      return cuenta.getSaldo() - getMonto();
-    }
+  public boolean fueDepositado(LocalDate fecha) {
+    return  this.esDeLaFecha(fecha);
   }
 }
+
+
+class Extraccion extends Movimiento{
+  public Extraccion(LocalDate fecha, double monto) {
+    super(fecha, monto);
+  }
+  public double calcularValor(double saldo){
+    return saldo - this.getMonto();
+  }
+  public boolean fueExtraido(LocalDate fecha) {
+    return this.esDeLaFecha(fecha);
+  }
+
+
+
+
+
+}
+
